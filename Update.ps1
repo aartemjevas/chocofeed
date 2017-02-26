c6edf5613c8e8829099d551584421ff1b2027bf7
Aleksandras Artemjevas <aleksandras.artemjevas@runwaybpo.com>
2017-02-26 16:18:03 +0200

    updating update script

--- Update.ps1
+++ Update.ps1
@@ -1,5 +1,4 @@
-﻿git clone https://github.com/aartemjevas/ChocoPackageUpdater.git
-Import-Module .\ChocoPackageUpdater\ChocoPackageUpdater.psm1
+﻿Import-Module $PSScriptRoot\ChocoPackageUpdater\ChocoPackageUpdater.psm1
 
 foreach ($packagePath in Get-ChildItem -Path .\Packages) {
     $packages += Get-ChocoPackage -Path $packagePath.fullname

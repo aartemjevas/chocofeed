df9f90aca01d1dcc34db629ef4da8ecfac4b1e4c
Aleksandras Artemjevas <aleksandras.artemjevas@runwaybpo.com>
2017-02-26 16:23:25 +0200

    updating update script

--- Update.ps1
+++ Update.ps1
@@ -1,5 +1,7 @@
 ﻿Import-Module $PSScriptRoot\ChocoPackageUpdater\ChocoPackageUpdater.psm1
 
+$packages = @()
+$env:ChocoPackageSource = 'https://www.myget.org/F/chocofeed/api/v2'
 foreach ($packagePath in Get-ChildItem -Path .\Packages) {
     $packages += Get-ChocoPackage -Path $packagePath.fullname
 }
